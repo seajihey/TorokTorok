ebe0dedb0755869fe2f2d240ba878682ba98dcb3
uooonth <dbstjd1576@gmail.com>
2023-12-10 19:34:59 +0900

    backgroundcolor

--- js/color.js
+++ js/color.js
@@ -1,39 +1,19 @@
-function setTheme(name) {
-    document.querySelector(`.${name}`).onclick = () => {
-        const root = document.documentElement;
-        root.style.setProperty('--primary-color', `var(--${name}-color-10)`);
-        root.style.setProperty('--primary-hover-color', `var(--${name}-color)`);
-        root.style.setProperty('--primary-border-color', `var(--${name}-color)`);
-    };
-}
-
 function setWhiteLabeling() {
-    document.querySelector('.whiteLabel-revert').onclick = () => {
-        const btns = document.querySelectorAll('.btn-primary');
-        btns.forEach(btn => {
-            btn.style.setProperty('--background-color', 'var(--primary-color)');
-            btn.style.setProperty('--hover-bg-color', 'var(--primary-hover-color)');
-            btn.style.setProperty('--color', 'var(--text-on-dark-color)');
-        });
-    };
-
-    document.querySelector('.whiteLabel-color').onchange = event => {
+    
+    document.querySelector('.main-color').onchange = event => {
         const btns = document.querySelectorAll('.btn-primary');
         btns.forEach(btn => btn.style.setProperty('--background-color', event.target.value));
     };
     
-    document.querySelector('.whiteLabel-hover').onchange = event => {
+    document.querySelector('.sub-color').onchange = event => {
         const btns = document.querySelectorAll('.btn-primary');
         btns.forEach(btn => btn.style.setProperty('--hover-bg-color', event.target.value));
     };
     
-    document.querySelector('.whiteLabel-text').onchange = event => {
+    document.querySelector('.back-color').onchange = event => {
         const btns = document.querySelectorAll('.body');
-        btns.forEach(btn => btn.style.setProperty('--color', event.target.value));
+        btns.forEach(btn => btn.style.setProperty('--background-color', event.target.value));
     };
 }
 
-setTheme('green');
-setTheme('red');
-setTheme('blue');
 setWhiteLabeling();
