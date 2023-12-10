675bc2bacd3c984f3e15a0f4df8a2e90f509b320
uooonth <dbstjd1576@gmail.com>
2023-12-10 20:37:40 +0900

    textcolor

--- js/color.js
+++ js/color.js
@@ -6,14 +6,10 @@ function setWhiteLabeling() {
     };
     
     document.querySelector('.sub-color').onchange = event => {
-        const btns = document.querySelectorAll('.btn-primary');
-        btns.forEach(btn => btn.style.setProperty('--hover-bg-color', event.target.value));
+        const btns = document.querySelectorAll('.body');
+        btns.forEach(btn => btn.style.setProperty('color', event.target.value));
     };
     
-    document.querySelector('.back-color').onchange = event => {
-        const btns = document.querySelectorAll('.body');
-        btns.forEach(btn => btn.style.setProperty('--background-color', event.target.value));
-    };
 }
 
 setWhiteLabeling();
